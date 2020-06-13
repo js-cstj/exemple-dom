539bdf5728af06ea8b19c0f14ec281a24d3e73f9
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-06-13 10:05:50 -0400

    select et radio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,44 @@ export default class App {
 	 */
 	static main() {
 		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		document.getElementById("app").appendChild(this.html_select());
+		document.getElementById("app").appendChild(this.html_radiogroup());
+	}
+	static html_select() {
+		var select = document.createElement("select");
+		select.setAttribute("name", "taille");
+		select.appendChild(this.html_option("xs", "Extra Small"));
+		select.appendChild(this.html_option("s", "Small"));
+		select.appendChild(this.html_option("m", "Medium"));
+		select.appendChild(this.html_option("l", "Large"));
+		select.appendChild(this.html_option("xl", "Extra Large"));
+		return select;
+	}
+	static html_option(value, text) {
+		var option = document.createElement("option");
+		option.setAttribute("value", value);
+		option.innerHTML = text;
+		return option;
+	}
+	static html_radiogroup() {
+		var fieldset = document.createElement("fieldset");
+		fieldset.setAttribute("name", "taille");
+		fieldset.appendChild(this.html_radio("xs", "Extra Small"));
+		fieldset.appendChild(this.html_radio("s", "Small"));
+		fieldset.appendChild(this.html_radio("m", "Medium"));
+		fieldset.appendChild(this.html_radio("l", "Large"));
+		fieldset.appendChild(this.html_radio("xl", "Extra Large"));		
+		return fieldset;
+	}
+	static html_radio(value, text) {
+		var label = document.createElement("label");
+		var input = label.appendChild(document.createElement("input"));
+		input.setAttribute("type", "radio");
+		input.setAttribute("name", "taille");
+		input.setAttribute("id", "taille");
+		input.setAttribute("value", value);
+		label.appendChild(document.createTextNode(text));
+		return label;
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
