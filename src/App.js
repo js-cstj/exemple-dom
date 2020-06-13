60910903f4296b5c03ba0094675c274d937f61db
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-06-13 10:34:03 -0400

    feux couleurs simples

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /*jslint esnext:true, browser:true*/
+
 /**
  * @module App
  */
@@ -8,7 +9,31 @@ export default class App {
 	 */
 	static main() {
 		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		document.getElementById("app").appendChild(this.html_feuxCirculation());
+	}
+	static html_feuxCirculation() {
+		var resultat = document.createElement("table");
+		resultat.style.borderSpacing = "1em";
+		resultat.style.backgroundColor = "#333";
+		var tbody = resultat.appendChild(document.createElement("tbody"));
+		var tr = tbody.appendChild(document.createElement("tr"));
+		var td = tr.appendChild(document.createElement("td"));
+		td.appendChild(this.html_feu("hsl(0, 100%, 50%)"));
+		var tr = tbody.appendChild(document.createElement("tr"));
+		var td = tr.appendChild(document.createElement("td"));
+		td.appendChild(this.html_feu("hsl(60, 100%, 50%)"));
+		var tr = tbody.appendChild(document.createElement("tr"));
+		var td = tr.appendChild(document.createElement("td"));
+		td.appendChild(this.html_feu("hsl(120, 100%, 50%)"));
+		return resultat;
+	}
+	static html_feu(couleur) {
+		var div = document.createElement("div");
+		div.style.borderRadius = "100%";
+		div.style.width = "3em";
+		div.style.height = "3em";
+		div.style.backgroundColor = couleur;
+		return div;
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
