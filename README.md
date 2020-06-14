535aa86e374d87800115b39c98aa0f154d0bd61e
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-06-14 13:15:55 -0400

    Menu d'un objet; avec readme

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
-# application-base
-Application de base pour débuter un projet
+# Le menu DOM
+Recréer un menu à partir d'un objet générique.
 
-## Description
-Description à venir
+## Objet générique
+```javascript
+var principal = {
+    "Édition": "edition.html",
+    "Sélection": "selection.html",
+    "Affichage": "affichage.html",
+    "Fenêtre": "fenetre.html",
+    "Aide": "aide.html",
+}
+```
 
+## HTML final
+
+```html
+<nav class="principal">
+    <ul>
+        <li><a href="index.html">Accueil</a></li>
+        <li><a href="edition.html">Édition</a></li>
+        <li><a href="selection.html">Sélection</a></li>
+        <li><a href="affichage.html">Affichage</a></li>
+        <li><a href="fenetre.html">Fenêtre</a></li>
+        <li><a href="aide.html">Aide</a></li>
+        <li><a href="about.html">À propos de nous</a></li>
+    </ul>
+</nav>
+```
 ## Particularités
-
-- Compléter
-
-## Variantes et ajouts possibles
-
-- Compléter
+- Le projet nécessite 2 méthodes statiques : une pour le nav et une pour chaque li;
+- Le nom de la classe est fourni en paramêtre et pourrait être absent.
+- Le CSS est complet.
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /*jslint esnext:true, browser:true*/
+
 /**
  * @module App
  */
@@ -8,7 +9,35 @@ export default class App {
 	 */
 	static main() {
 		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		var principal = {
+			"Édition": "edition.html",
+			"Sélection": "selection.html",
+			"Affichage": "affichage.html",
+			"Fenêtre": "fenetre.html",
+			"Aide": "aide.html",
+		}
+		document.getElementById("app").appendChild(this.html_nav(principal, "principal"));
+	}
+	static html_nav(elements, classe) {
+		var resultat = document.createElement("nav");
+		var ul = resultat.appendChild(document.createElement("ul"));
+		if (classe) {
+			resultat.classList.add(classe);
+		}
+
+		ul.appendChild(this.html_li("index.html", "Accueil"));
+		for (let element in elements) {
+			ul.appendChild(this.html_li(elements[element], element));
+		}
+		ul.appendChild(this.html_li("about.html", "À propos de nous"));
+		return resultat;
+	}
+	static html_li(url, etiquette) {
+		var resultat = document.createElement("li");
+		var a = resultat.appendChild(document.createElement("a"));
+		a.setAttribute("href", url);
+		a.innerHTML = etiquette;
+		return resultat;
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
