f9c00bb78a1f0223f92ef34ceba185e79fa26e39
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-06-14 12:24:08 -0400

    liste (3 array)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /*jslint esnext:true, browser:true*/
+
 /**
  * @module App
  */
@@ -7,8 +8,63 @@ export default class App {
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		var jours = [
+			"dimanche", 
+			"lundi", 
+			"mardi", 
+			"mercredi", 
+			"jeudi", 
+			"vendredi", 
+			"samedi",
+		];
+		var mois = [
+			"janvier", 
+			"février", 
+			"mars", 
+			"avril", 
+			"mai", 
+			"juin", 
+			"juillet",
+			"août",
+			"septembre",
+			"octobre",
+			"novembre",
+			"decembre",
+		];
+		var signes = [
+			"bélier", 
+			"taureau", 
+			"gémeaux", 
+			"cancer", 
+			"lion", 
+			"vierge", 
+			"balance",
+			"scorpion",
+			"sagittaire",
+			"capricorne",
+			"verseau",
+			"poissons",
+		];
+		document.getElementById("app").appendChild(this.html_liste(jours));
+		document.getElementById("app").appendChild(this.html_liste(mois));
+		document.getElementById("app").appendChild(this.html_liste(signes));
+	}
+	static html_liste(elements, ordonnee = false) {
+		var resultat;
+		if (ordonnee) {
+			resultat = document.createElement("ol");
+		} else {
+			resultat = document.createElement("ul");
+		}
+		elements.forEach(element => {
+			resultat.appendChild(this.html_li(element));
+		});
+		return resultat;
+	}
+	static html_li(contenu) {
+		var resultat = document.createElement("li");
+		resultat.innerHTML = contenu;
+		return resultat;
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
