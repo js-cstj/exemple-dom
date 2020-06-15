493b658d712ccd63b1e56e1ac396c17b3878541b
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-06-14 20:38:26 -0400

    Tableau stats données complexes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,70 @@
 /**
  * @module App
  */
+import {stats} from "./stats.js";
 export default class App {
 	/**
 	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
 	 */
 	static main() {
-		console.log("Je suis prêt");
-		document.getElementById("app").innerHTML = "La page est chargée";
+		document.getElementById("app").appendChild(this.html_stats(stats));
+	}
+	static html_stats(stats) {
+		var resultat = document.createElement("table");
+
+		resultat.classList.add("stats");
+		resultat.appendChild(this.html_colgroup(stats.colonnes));
+		resultat.appendChild(this.html_thead(stats.colonnes));
+		resultat.appendChild(this.html_tbody(stats));
+		return resultat;
+	}
+	static html_colgroup(colonnes) {
+		var resultat = document.createElement("colgroup");
+		colonnes.forEach(colonne => {
+			var col = resultat.appendChild(document.createElement("col"));
+			col.setAttribute("id", "col-" + colonne.id);
+		});
+		return resultat;
+	}
+	static html_thead(colonnes) {
+		var resultat = document.createElement("thead");
+		var tr = resultat.appendChild(document.createElement("tr"));
+		colonnes.forEach(colonne => {
+			var th = tr.appendChild(document.createElement("th"));
+			th.setAttribute("title", colonne.titre);
+			th.setAttribute("data-for", colonne.id);
+			th.innerHTML = colonne.label;
+		});
+		return resultat;
+	}
+	static html_tbody(stats) {
+		var resultat = document.createElement("tbody");
+
+		stats.equipes.forEach(statsEquipe => {
+			resultat.appendChild(this.html_equipe(statsEquipe, stats.colonnes));
+		});
+		return resultat;
+	}
+	static html_equipe(statsEquipe, colonnes) {
+		var resultat = document.createElement("tr");
+		colonnes.forEach(colonne => {
+			if (colonne.th) {
+				var cell = resultat.appendChild(document.createElement("th"));
+			} else {
+				var cell = resultat.appendChild(document.createElement("td"));
+			}
+			this.ajouterStyle(cell, colonne.style);
+			cell.innerHTML = statsEquipe[colonne.id];
+		});
+		return resultat;
+	}
+	static ajouterStyle(objHtml, style) {
+		if (!style) {
+			return;
+		}
+		for(let prop in style) {
+			objHtml.style.setProperty(prop, style[prop]);
+		}
 	}
 	/**
 	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
